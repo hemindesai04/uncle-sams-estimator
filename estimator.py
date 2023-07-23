710d681e0983164de733782182283057475c7198
Hemin Desai <hemin_desai04@yahoo.com>
2023-07-23 18:29:42 -0400

    initial commit

---
+++ estimator.py
@@ -0,0 +1,22 @@
+
+def calculate_estimated_tax() -> float:
+    """
+    Function to calculate the estimated total tax
+
+    :return:
+    Total estimated tax
+    """
+    # verify_pnii()
+    # verify_income()
+    # verify_pretax_contributions()
+    # estimate_agi()
+    # get_standard_deduction(filing_status)
+    # estimate_taxable_income()
+    # estimate_fed_tax()
+    # estimate_state_tax()
+    # estimate_fica_tax()
+    return 0.0
+
+
+if __name__ == "__main__":
+    calculate_estimated_tax()
